75bb02b296dbddf6efa1408319c4e51a3bfcd978
Hargunbeer Singh <hargunbeersingh@gmail.com>
2022-01-10 13:26:44 +0530

    Added various new colorschemes

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -73,6 +73,10 @@ return packer.startup(function(use)
   use "lunarvim/darkplus.nvim"
   use "folke/tokyonight.nvim"
   use "wadackel/vim-dogrun"
+  use "EdenEast/nightfox.nvim"
+  use "sainnhe/everforest"
+  use "shaunsingh/moonlight.nvim"
+  use "navarasu/onedark.nvim"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
@@ -81,7 +85,6 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/nvim-cmp" --completion
  	use {'tzachar/cmp-tabnine', run='./install.sh', requires = 'hrsh7th/nvim-cmp'}
 
   -- snippets
